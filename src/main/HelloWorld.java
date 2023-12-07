b4ee65fcb60d0bd0b7b12279db2d321fd4e53719
11038278 <sfy_@2021>
2023-12-07 09:57:12 +0800

    第二次提交代码

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -8,6 +8,7 @@ package main;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("第一次提交");
+//        System.out.println("第一次提交");
+        System.out.println("第二次提交");
     }
 }
