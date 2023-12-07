f43b040462c3d65a295c8b14fcb11fb8815ef93b
11038278 <sfy_@2021>
2023-12-07 09:55:22 +0800

    第一次提交代码

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * HelloWorld
+ * @author xieBingLin
+ * @Description 解释
+ * @Date 2023/12/7 9:39
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
